4d5585bb3926de396ae10052865aaf3a8191b5f9
Josh Chen <qc588@nyu.edu>
2022-11-21 12:07:52 -0500

    change3

--- love machine/love machine.ino
+++ love machine/love machine.ino
@@ -17,32 +17,24 @@ void setup() {
   pinMode(sensor1,INPUT);
   pinMode(sensor2,INPUT);
   
-  //randomNum = random(2,4);
+ 
   
 
 }
 void loop() {
   compare();
+  // previous error codes:
+   //randomNum = random(2,4);
   //int val1 = analogRead(sensor1);
   //int val2 = analogRead(sensor2);
   // Serial.println(val1);
   // Serial.println(val2);
 
-  // if(val2<=600){
-  //   digitalWrite(LEDPins[0], HIGH);
-  //   digitalWrite(LEDPins[1], LOW);
-  // } else {
-  //   digitalWrite(LEDPins[1], HIGH);
-  //   digitalWrite(LEDPins[0], LOW);
-  // }
-
     //if(val1<=600&&val2<=600){
       //digitalWrite(randomNum,HIGH);
      // Serial.println(randomNum);
    // } else if (val1>500&&val2>500){
-      //digitalWrite(randomNum, LOW);
-     // digitalWrite(randomNum, LOW);
-      
+      //digitalWrite(randomNum, LOW);  
     }
 
      
@@ -55,19 +47,20 @@ previous2=current2;
 current1= analogRead (sensor1);
 Serial.println(current1);
 current2= analogRead (sensor2);
-//while working, light up a random light
+//while wpressing: light up a random light, define current= "pressed state" as 1-->let current= pressed state=1
 if ( current1-previous1 >200 && current2-previous2 >200) {  
      current1=1;
      current2=1;
      digitalWrite(randomNum,HIGH);
     //shut down the light when off 
   } else {
+    //release: light off, define current as "released state" as 0--> let current = released state =0
 current1=0;
 current2=0;
 digitalWrite(LEDPins1, LOW);
 digitalWrite(LEDPins2, LOW);
 }
-//compare the states: when press, states become different, choose a random number for light
+//compare states: when press or release-> states become different: 1!=0, current !=previous-> choose a random number for light
 if (current1 !=previous1 && current2!=previous2 ) {
       randomNum = random(2,4);
       digitalWrite(5-randomNum, LOW);
